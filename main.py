322bd78692e6c6d2394f26596da4ffb55fc5c71d
Sachin <sachinacharya44@gmail.com>
2023-05-30 21:31:15 +0545

    Added post function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
@@ -7,15 +8,16 @@ class Post(BaseModel):
   content:str
   published:bool=True
   rating:Optional[int]=None
-
+my_posts=[{'title':'title of post 1','content':"Content of post 1","id":1}]
 @app.get("/")
 async def root():
     return {"message":"Welcome to  !!!! my api"}
 @app.get("/post")
 def get_post():
-    return {"id":"Sachin",
-            "data":"This is your post"}
-@app.post("/create-post")
-def create_post(pay_load:Post):
-    print(pay_load.dict())
-    
+    return {"data":my_posts}
+@app.post("/posts")
+def create_post(post:Post):
+  post_dict=post.dict()
+  post_dict['id']=random.randint(0,1000000)
+  my_posts.append(post_dict)
+  return(post_dict)
