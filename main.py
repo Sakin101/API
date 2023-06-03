b3bf4220f5d9cb761c897f2c4fbf1f56e138d690
Sachin <sachinacharya44@gmail.com>
2023-06-03 21:42:31 +0545

    Retrieve individual posts

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from pickle import NONE
 import random
 from typing import Optional
 from fastapi import Body, FastAPI
@@ -13,7 +15,7 @@ my_posts=[{'title':'title of post 1','content':"Content of post 1","id":1}]
 async def root():
     return {"message":"Welcome to  !!!! my api"}
 @app.get("/post")
-def get_post():
+def get_posts():
     return {"data":my_posts}
 @app.post("/posts")
 def create_post(post:Post):
@@ -21,3 +23,14 @@ def create_post(post:Post):
   post_dict['id']=random.randint(0,1000000)
   my_posts.append(post_dict)
   return(post_dict)
+@app.get("/posts/{id}")
+def get_post(id:int):
+  print(id)
+  post=find_post(id)
+  print(post)
+  return {f"{post}"}
+
+
+def find_post(id:int):
+  return next((data for data in my_posts if data["id"]==id), NONE)
+       
