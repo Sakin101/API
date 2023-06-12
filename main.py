9f5c501ec25e4bba8e0d18cce18b2065ca2dc432
Sachin Acharya <sachinacharya44@gmail.com>
2023-06-12 11:42:37 +0545

    Update post added

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Post(BaseModel):
   published:bool=True
   rating:Optional[int]=None
 my_posts=[{'title':'title of post 1','content':"Content of post 1","id":1},
-          {'title':'title of post 1','content':"Content of post 1","id":2}]
+          {'title':'title of post 1','content':"Content of post 2","id":2}]
 @app.get("/")
 def root():
     return {"message":"Welcome to  !!!! my api"}
@@ -36,17 +36,30 @@ def get_post(id:int,response:Response):
   if post is None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail=f"Post of {id } not found")
-       response.status_code=status.HTTP_404_NOT_FOUND
-       return {'message':f'Post with {id} not found'}
        
   return post
 
 @app.delete("/posts/{id}")
 def delete_post(id:int):
   index=find_index(id)
+  print(index)
   if index is None:
     HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not found")
-  my_posts.remove(index)
+  my_posts.pop(index)
+  return Response(status_code=status.HTTP_200_OK)
+@app.put("/posts/{id}")
+def update_post(id:int,post:Post):
+  index=find_index(id)
+  if index is None:
+    HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not found")
+  post=post.dict()
+  my_posts[index]["title"]=post['title']
+  my_posts[index]['content']=post['content']
+  return Response(status_code=status.HTTP_200_OK)
+  
+  
+  
+
 def find_index(id:int):
   return next((idx for idx, data in enumerate(my_posts) if data["id"]==id), None)  
 def find_post(id:int):
